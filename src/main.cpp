9f458f5529b07ebc5a374b98a999ce64ce4468a0
Tr33 <g-h-l-w@gmx.de>
2018-12-10 22:32:07 +0100

    New Code includes HT16K33

--- src/main.cpp
+++ src/main.cpp
@@ -1,75 +1,103 @@
 #include <Arduino.h>
-
-static const int LED_GREEN = 16;
-static const int LED_BLUE = 5; //D1
-static const int LED_RED = 4; //D2
-static const int SWITCH_1 = 0; // D3
-static const int SWITCH_GROUND_3 = 12; //D6
-static const int SWITCH_GROUND_4 = 14; //D5
-
-int dimm[4][4];
-bool sg[4][4];
-
-
-
+#include <Wire.h> 
+const uint8_t addr = 0x70; // HT16K33 default address
+uint16_t displayBuffer[8];
+ 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(LED_GREEN, OUTPUT);
-  pinMode(LED_BLUE, OUTPUT);
-  pinMode(LED_RED, OUTPUT);
-  pinMode(SWITCH_1, INPUT_PULLUP);
-
-  pinMode(SWITCH_GROUND_3, OUTPUT);
-  pinMode(SWITCH_GROUND_4, OUTPUT);
-  
-  digitalWrite(SWITCH_GROUND_3, HIGH); //[0][2] --> IF LOW
-  digitalWrite(SWITCH_GROUND_4, HIGH);  //[0][3] --> IF LOW
-  analogWrite(LED_GREEN, 40);
-  
-
-
+	Wire.begin();
+ 
+	Wire.beginTransmission(addr);
+	Wire.write(0x20 | 1); // turn on oscillator
+	Wire.endTransmission();
+ 
+	setBrightness(15);
+	blink(0);
 }
-
+ 
 void loop() {
-  // put your main code here, to run repeatedly:
- /*  digitalWrite(LED_BLUE, HIGH);
-  digitalWrite(LED_GREEN, HIGH);
-  digitalWrite(LED_RED, HIGH);
-  delay(1000);
-  digitalWrite(LED_BLUE, LOW);
-  digitalWrite(LED_GREEN, LOW);
-  digitalWrite(LED_RED, LOW);
-  delay(1000); */
-  digitalWrite(SWITCH_GROUND_3, LOW);
-  digitalWrite(SWITCH_GROUND_4, HIGH);
-
-  sg[0][2] = !digitalRead(SWITCH_1);
-  
-  analogWrite(LED_BLUE, dimm[0][2]);
-  if (sg[0][2]) {
-    dimm[0][2] = 1024; 
-  }
-  if (dimm[0][2] > 0) {
-    dimm[0][2]--;
-    delayMicroseconds(100);
-
-  }
-  
-  digitalWrite(SWITCH_GROUND_3, HIGH);
-  digitalWrite(SWITCH_GROUND_4, LOW);
-
-  sg[0][3] = !digitalRead(SWITCH_1);
-
-  analogWrite(LED_RED, dimm[0][3]);
-  if (sg[0][3]) {
-    dimm[0][3] = 1024; 
-  }
-  if (dimm[0][3] > 0) {
-    dimm[0][3]--;
-    delayMicroseconds(100);
-
-  }
-  
-  
-
+	const int dTime = 50;
+ 
+	// Loop through all segments
+	for(int i = 0; i < 8; i++){
+		for(int k = 0; k < 16; k++){
+		  displayBuffer[i] = 1 << k;
+		  show();
+		  delay(dTime);
+		}
+		clear();
+	}
+ 
+	// Turn on all segments, one at a time
+	for(int i = 0; i < 8; i++){
+		for(int k = 0; k < 16; k++){
+			displayBuffer[i] |= 1 << k;
+			show();
+			delay(dTime);
+		}
+	}
+ 
+	// Test blinking
+	for(int i = 3 ; i > 0; i--){
+		blink(i);
+		delay(2000);
+	}
+	blink(0); // Turn blinking off
+ 
+	// Test blanking
+	for(int i = 0; i < 10; i++){
+		blank();
+		delay(dTime * 2);
+	}
+ 
+	// Test dimming
+	for(int i = 15; i >= 0; i--){
+		setBrightness(i);
+		delay(dTime * 2);
+	}
+	clear();
+ 
+	setBrightness(15);
+}
+ 
+void show(){
+	Wire.beginTransmission(addr);
+	Wire.write(0x00); // start at address 0x0
+ 
+	for (int i = 0; i < 8; i++) {
+		Wire.write(displayBuffer[i] & 0xFF);    
+		Wire.write(displayBuffer[i] >> 8);    
+	}
+	Wire.endTransmission();  
+}
+ 
+void clear(){
+	for(int i = 0; i < 8; i++){
+		displayBuffer[i] = 0;
+	}
+}
+ 
+void setBrightness(uint8_t b){
+	if(b > 15) return;
+ 
+	Wire.beginTransmission(addr);
+	Wire.write(0xE0 | b); // Dimming command
+	Wire.endTransmission();
+}
+ 
+void blank(){
+	static boolean blankOn;  
+ 
+	Wire.beginTransmission(addr);
+	Wire.write(0x80 | blankOn); // Blanking / blinking command
+	Wire.endTransmission();
+ 
+	blankOn = !blankOn;
+}
+ 
+void blink(uint8_t b){
+	if(b > 3) return;
+ 
+	Wire.beginTransmission(addr);
+	Wire.write(0x80 | b << 1 | 1); // Blinking / blanking command
+	Wire.endTransmission();
 }
